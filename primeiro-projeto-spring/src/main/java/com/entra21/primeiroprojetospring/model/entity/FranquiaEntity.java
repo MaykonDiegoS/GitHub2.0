a2fb0a9c8731c22a3ba5653d4509cc4df60d1801
Maykon Diego Santos da Silva <mdssilva@furb.br>
2022-09-01 21:35:28 -0300

    franquia

--- primeiro-projeto-spring/src/main/java/com/entra21/primeiroprojetospring/model/entity/FranquiaEntity.java
+++ primeiro-projeto-spring/src/main/java/com/entra21/primeiroprojetospring/model/entity/FranquiaEntity.java
@@ -1,4 +1,21 @@
 package com.entra21.primeiroprojetospring.model.entity;
 
-public class FranquiaEntity {
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "franquia")
+public class FranquiaEntity{
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+
+
 }
